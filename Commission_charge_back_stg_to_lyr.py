6c0de3cdc470442fb723822615b0d8d9650869aa
himanshubansal1452 <137521792+himanshubansal1452@users.noreply.github.com>
2023-06-23 20:22:22 +0530

    Update Commission_charge_back_stg_to_lyr.py

--- Commission_charge_back_stg_to_lyr.py
+++ Commission_charge_back_stg_to_lyr.py
@@ -4,6 +4,7 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
+from awsglue.job import *
 
 args = getResolvedOptions(sys.argv, ["JOB_NAME"])
 sc = SparkContext()
